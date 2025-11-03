7f41dc785c4e9612ac5774822e230288a1b346d0
Hypergamer6899A <bj951630@gmail.com>
2025-11-03 12:59:19 -0600

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,1 +1,18 @@
+import { SlashCommandBuilder } from "discord.js";
+import fs from "fs";
 
+export const data = new SlashCommandBuilder()
+  .setName("balance")
+  .setDescription("Check your balance");
+
+export async function execute(interaction) {
+  const filePath = "./data/balances.json";
+  if (!fs.existsSync(filePath)) fs.writeFileSync(filePath, "{}");
+  const balances = JSON.parse(fs.readFileSync(filePath));
+  const id = interaction.user.id;
+
+  if (!balances[id]) balances[id] = 1000;
+  fs.writeFileSync(filePath, JSON.stringify(balances, null, 2));
+
+  await interaction.reply(`${interaction.user.username}, you have $${balances[id].toLocaleString()}.`);
+}
