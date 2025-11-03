20b81a617c25caf7e73e3d352a194edc7ac801a3
Hypergamer6899A <bj951630@gmail.com>
2025-11-03 15:38:17 -0600

    Update topRoles.js

--- topRoles.js
+++ topRoles.js
@@ -5,15 +5,20 @@ const TOP_ROLE_ID = "1434989027555016755";
 
 export async function updateTopRoles(client) {
   try {
+    if (!client) throw new Error("Client not provided to updateTopRoles");
+
     const guild = await client.guilds.fetch(GUILD_ID);
     await guild.members.fetch();
 
+    // Get top 3 users by balance
     const snapshot = await db.collection("users").orderBy("balance", "desc").limit(3).get();
     const topUsers = snapshot.docs.map(doc => doc.id);
 
     // Remove top role from everyone
     for (const member of guild.members.cache.values()) {
-      if (member.roles.cache.has(TOP_ROLE_ID)) await member.roles.remove(TOP_ROLE_ID).catch(() => {});
+      if (member.roles.cache.has(TOP_ROLE_ID)) {
+        await member.roles.remove(TOP_ROLE_ID).catch(() => {});
+      }
     }
 
     // Assign top role to top 3
@@ -21,6 +26,7 @@ export async function updateTopRoles(client) {
       const member = await guild.members.fetch(id).catch(() => null);
       if (member) await member.roles.add(TOP_ROLE_ID).catch(() => {});
     }
+
   } catch (err) {
     console.error("Error updating top roles:", err);
   }
