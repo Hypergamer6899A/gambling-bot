6b8095fbb8109f1966123fdef0f138419a928ccb
Hypergamer6899A <bj951630@gmail.com>
2025-11-03 15:04:06 -0600

    Create topRoles.js

---
+++ commands/topRoles.js
@@ -0,0 +1,30 @@
+import { db } from "../firebase.js";
+
+export async function updateTopRoles(client, GUILD_ID, TOP_ROLE_ID) {
+  try {
+    const guild = await client.guilds.fetch(GUILD_ID);
+    await guild.members.fetch();
+
+    const snapshot = await db.collection("users")
+      .orderBy("balance", "desc")
+      .limit(3)
+      .get();
+
+    const topUsers = snapshot.docs.map(doc => doc.id);
+
+    // Remove top role from everyone
+    for (const member of guild.members.cache.values()) {
+      if (member.roles.cache.has(TOP_ROLE_ID)) {
+        await member.roles.remove(TOP_ROLE_ID).catch(() => {});
+      }
+    }
+
+    // Assign top role to top 3
+    for (const id of topUsers) {
+      const member = await guild.members.fetch(id).catch(() => null);
+      if (member) await member.roles.add(TOP_ROLE_ID).catch(() => {});
+    }
+  } catch (err) {
+    console.error("Error updating top roles:", err);
+  }
+}
