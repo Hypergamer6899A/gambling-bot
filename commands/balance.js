6e6c62fae76dcc0a17e2a81b12538f8277393cf0
Hypergamer6899A <bj951630@gmail.com>
2025-11-03 14:44:04 -0600

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,14 +1,13 @@
 import { SlashCommandBuilder } from "discord.js";
 import { db } from "../firebase.js";
 
-const ALLOWED_CHANNEL_ID = "1434934862430867487"; // same as index.js
+const ALLOWED_CHANNEL_ID = "1434934862430867487";
 
 export const data = new SlashCommandBuilder()
   .setName("balance")
   .setDescription("Check your balance");
 
 export async function execute(interaction) {
-  // Restrict to channel
   if (interaction.channel.id !== ALLOWED_CHANNEL_ID) {
     return interaction.reply({
       content: `You can only use this command in <#${ALLOWED_CHANNEL_ID}>.`,
@@ -24,5 +23,5 @@ export async function execute(interaction) {
   if (doc.exists) balance = doc.data().balance;
   else await ref.set({ balance });
 
-  await interaction.reply(`${interaction.user.username}, you have $${balance.toLocaleString()}.`);
+  await interaction.reply(`<@${id}>, you have $${balance.toLocaleString()}.`);
 }
