665e7bc803f6b5d2a44b0bea8ed5c5a70e745e12
Hypergamer6899A <bj951630@gmail.com>
2025-12-11 11:56:05 -0600

    Update balance.js

--- commands/g/balance.js
+++ commands/g/balance.js
@@ -1,1 +1,16 @@
+import { getUser } from "../../services/userCache.js";
+import { EmbedBuilder } from "discord.js";
+
+export async function balanceCommand(client, message) {
+  const user = await getUser(message.author.id);
+
+  return message.reply({
+    embeds: [
+      new EmbedBuilder()
+        .setColor("Blurple")
+        .setTitle("Your Balance")
+        .setDescription(`You currently have **$${user.balance}**.`)
+    ]
+  });
+}
 
