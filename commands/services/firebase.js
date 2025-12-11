87144fe70d59d0eefe31b2198f7c492b06dcd57a
Hypergamer6899A <bj951630@gmail.com>
2025-12-11 11:55:50 -0600

    Update firebase.js

--- commands/services/firebase.js
+++ commands/services/firebase.js
@@ -1,1 +1,20 @@
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+if (!process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
+  console.error("Missing Firebase environment variables");
+  process.exit(1);
+}
+
+initializeApp({
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  })
+});
+
+console.log("Firebase initialized.");
+
+export const db = getFirestore();
 
