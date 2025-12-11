9aff612e60538202b820b9afafcea5a0c374e970
Hypergamer6899A <bj951630@gmail.com>
2025-12-11 12:28:44 -0600

    Update help.js

--- commands/g/help.js
+++ commands/g/help.js
@@ -1,4 +1,4 @@
-import { helpEmbed } from "../../utils/helpEmbed.js";
+import { helpEmbed } from "/../utils/helpEmbed.js";
 
 export function helpCommand(client, message) {
   return message.reply({ embeds: [helpEmbed()] });
