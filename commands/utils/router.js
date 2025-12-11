0c0b0d5f2f38e5fd91fa9e7af0e5af154b8d7494
Hypergamer6899A <bj951630@gmail.com>
2025-12-11 12:13:15 -0600

    Update router.js

--- commands/utils/router.js
+++ commands/utils/router.js
@@ -5,14 +5,10 @@ import { leaderboardCommand } from "../commands/g/leaderboard.js";
 import { balanceCommand } from "../commands/g/balance.js";
 import { giftCommand } from "../commands/g/gift.js";
 import { helpCommand } from "../commands/g/help.js";
-import { rouletteCommand } from "../commands/g/roulette.js";
-import { leaderboardCommand } from "../commands/g/leaderboard.js";
 
-
-// UNO subcommands
+// UNO subcommands (if needed in future)
 import { startUnoCollector } from "../commands/uno/collector.js";
 
-// Prefix groups
 const PREFIX = "!g";
 const UNO_PREFIX = "!uno";
 
@@ -23,8 +19,7 @@ export function messageRouter(client, message) {
 
   // UNO sub-commands
   if (content.toLowerCase().startsWith(UNO_PREFIX)) {
-    // These get handled INSIDE the UNO game channel collector
-    return;
+    return; // handled inside UNO collector
   }
 
   // Main command prefix
@@ -36,31 +31,18 @@ export function messageRouter(client, message) {
   switch (cmd) {
     case "uno":
       return unoStart(client, message, args);
-
     case "blackjack":
       return blackjackCommand(client, message, args);
-
     case "roulette":
       return rouletteCommand(client, message, args);
-
     case "leaderboard":
       return leaderboardCommand(client, message, args);
-      
     case "balance": 
       return balanceCommand(client, message);
-      
     case "gift": 
       return giftCommand(client, message, args);
-      
     case "help": 
       return helpCommand(client, message);
-      
-    case "roulette":
-      return rouletteCommand(client, message, args);
-      
-    case "leaderboard": 
-      return leaderboardCommand(client, message, args);
-
     default:
       return message.reply("Unknown command. Use `!g help`.");
   }
