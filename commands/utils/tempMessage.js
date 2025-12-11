c2afe1a83b2adee41fc65d95c5dbdea9e62a1064
Hypergamer6899A <bj951630@gmail.com>
2025-12-11 11:45:48 -0600

    Update tempMessage.js

--- commands/utils/tempMessage.js
+++ commands/utils/tempMessage.js
@@ -1,1 +1,6 @@
+export async function temp(channel, text, ttl = 3500) {
+  const msg = await channel.send(text).catch(()=>null);
+  if (!msg) return;
+  setTimeout(() => msg.delete().catch(()=>{}), ttl);
+}
 
