8574e3baf45f328463991edd3707abd16e84bf7c
Hypergamer6899A <bj951630@gmail.com>
2025-12-12 12:53:02 -0600

    Update helpEmbed.js

--- commands/utils/helpEmbed.js
+++ commands/utils/helpEmbed.js
@@ -13,6 +13,7 @@ export function helpEmbed() {
       "`!g roulette <red|black|odd|even> <amount>`\n" +
       "`!g uno <amount>`\n\n" +
       "**Misc**\n" +
+      "`!g claim` - Claim $100 every 24 hour when broke\n" +
       "`!g leaderboard` — Top balances\n" +
       "`!g help` — Show this menu"
     );
