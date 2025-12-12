9344eabe32dd3b119d960b2d5ec51fb301f195a7
Hypergamer6899A <bj951630@gmail.com>
2025-12-12 13:57:15 -0600

    Delete commands/games/uno directory

--- commands/games/uno/validate.js
+++
@@ -1,7 +0,0 @@
-// /games/uno/validate.js
-export function canPlayCard(card, topColor, topValue) {
-  if (card.color === "Wild") return true;
-  if (card.color === topColor) return true;
-  if (card.value === topValue) return true;
-  return false;
-}
