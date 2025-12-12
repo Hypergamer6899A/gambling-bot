cc2fa35a85a9aa21b9487cc11ce5c005b50199c2
Hypergamer6899A <bj951630@gmail.com>
2025-12-12 13:35:06 -0600

    Delete commands/games/uno directory

--- commands/games/uno/shuffle.js
+++
@@ -1,9 +0,0 @@
-// commands/utils/shuffle.js
-export function shuffle(array) {
-  // Fisher-Yates shuffle
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
