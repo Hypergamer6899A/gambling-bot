7884af3402a4b772b17358537a92552a78d563f2
Hypergamer6899A <bj951630@gmail.com>
2025-12-12 13:35:19 -0600

    Delete commands/utils/tempMessage.js

--- commands/utils/tempMessage.js
+++
@@ -1,6 +0,0 @@
-export async function temp(channel, text, ttl = 3500) {
-  const msg = await channel.send(text).catch(()=>null);
-  if (!msg) return;
-  setTimeout(() => msg.delete().catch(()=>{}), ttl);
-}
-
