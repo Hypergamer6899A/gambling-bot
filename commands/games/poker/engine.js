d57f17638bdd5d9b0157bc4ad3deae1ec6cb98ca
Hypergamer6899A <bj951630@gmail.com>
2026-02-17 13:15:25 -0600

    Create engine.js

---
+++ commands/games/poker/engine.js
@@ -0,0 +1,45 @@
+// src/commands/games/poker/engine.js
+
+import { makeDeck, shuffle, draw, scoreHand } from "./utils.js";
+
+export function newPokerGame() {
+  const deck = makeDeck();
+  shuffle(deck);
+
+  return {
+    deck,
+    board: draw(deck, 5),
+    playerCards: draw(deck, 5),
+    botCards: draw(deck, 5),
+    chosen: [],
+    finished: false
+  };
+}
+
+export function botPick(botCards) {
+  // Bot simply chooses first 3 for now
+  return botCards.slice(0, 3);
+}
+
+export function finishGame(game) {
+  const botChosen = botPick(game.botCards);
+
+  const playerFinal = [...game.chosen, ...game.board];
+  const botFinal = [...botChosen, ...game.board];
+
+  const playerScore = scoreHand(playerFinal);
+  const botScore = scoreHand(botFinal);
+
+  return {
+    playerFinal,
+    botFinal,
+    playerScore,
+    botScore,
+    winner:
+      playerScore.rank > botScore.rank
+        ? "player"
+        : playerScore.rank < botScore.rank
+        ? "bot"
+        : "tie"
+  };
+}
