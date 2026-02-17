b9ec08ad3166bf911d8b099aca801b98ae8fe4a1
Hypergamer6899A <bj951630@gmail.com>
2026-02-17 13:22:54 -0600

    Create embedColors.js

---
+++ commands/utils/embedColors.js
@@ -0,0 +1,13 @@
+// src/commands/utils/embedColors.js
+
+export const GAME_COLORS = {
+  WIN: "Green",
+  LOSS: "Red",
+  TIE: "Yellow"
+};
+
+export const UTIL_COLORS = {
+  BLUE: "Blue",
+  PURPLE: "Purple",
+  BLURPLE: "Blurple"
+};
