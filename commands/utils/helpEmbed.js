6b9c3e051b7e680f0a8f51df73504646905f6623
Hypergamer6899A <bj951630@gmail.com>
2026-02-17 09:41:38 -0600

    Update helpEmbed.js

--- commands/utils/helpEmbed.js
+++ commands/utils/helpEmbed.js
@@ -2,18 +2,20 @@ import { EmbedBuilder } from "discord.js";
 
 export function helpEmbed() {
   return new EmbedBuilder()
-    .setColor("Gold")
     .setTitle("Gambler — Command List")
+    .setColor("Purple")
     .setDescription(
-      "**Economy**\n" +
-      "`!g balance` — Check your balance\n" +
-      "`!g gift <user> <amount>` — Gift money\n\n" +
-      "**Games**\n" +
-      "`!g blackjack <amount>`\n" +
-      "`!g roulette <red|black|odd|even> <amount>`\n" +
-      "**Misc**\n" +
-      "`!g claim` - Claim $100 every 24 hour when broke\n" +
-      "`!g leaderboard` — Top balances\n" +
-      "`!g help` — Show this menu"
+      `**Economy**\n` +
+      `\`!g balance\` — Check your balance\n` +
+      `\`!g gift <user> <amount>\` — Gift money\n\n` +
+
+      `**Games**\n` +
+      `\`!g blackjack <amount>\`\n` +
+      `\`!g roulette <red|black|odd|even> <amount>\`\n\n` +
+
+      `**Misc**\n` +
+      `\`!g claim\` — Claim $100 every 24h when broke\n` +
+      `\`!g leaderboard\` — Top balances\n` +
+      `\`!g help\` — Show this menu`
     );
 }
