dc85aa17be7bdeb129c96e9d4d076b8efca4b300
Hypergamer6899A <bj951630@gmail.com>
2026-02-17 20:26:38 -0600

    Update embedColors.js

--- commands/utils/embedColors.js
+++ commands/utils/embedColors.js
@@ -1,13 +1,15 @@
 // src/commands/utils/embedColors.js
 
+// Discord.js v14 requires hex color values, not names.
+
 export const GAME_COLORS = {
-  WIN: "Green",
-  LOSS: "Red",
-  TIE: "Yellow"
+  WIN: 0x57F287,   // Green
+  LOSS: 0xED4245,  // Red
+  TIE: 0xFEE75C    // Yellow
 };
 
 export const UTIL_COLORS = {
-  BLUE: "Blue",
-  PURPLE: "Purple",
-  BLURPLE: "Blurple"
+  BLUE: 0x3498DB,     // Blue
+  PURPLE: 0x9B59B6,   // Purple
+  BLURPLE: 0x5865F2   // Discord Blurple
 };
