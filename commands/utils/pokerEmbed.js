3ed7472b3a7dc57d0eda311b2aa567faed5269f1
Hypergamer6899A <bj951630@gmail.com>
2026-02-17 20:29:04 -0600

    Update pokerEmbed.js

--- commands/utils/pokerEmbed.js
+++ commands/utils/pokerEmbed.js
@@ -1,11 +1,12 @@
 // src/commands/utils/pokerEmbed.js
 
 import { EmbedBuilder } from "discord.js";
+import { GAME_COLORS } from "./embedColors.js";
 
 export function pokerEmbed(title, bet, board, playerCards, chosen, status) {
   return new EmbedBuilder()
     .setTitle(title)
-    .setColor("Green")
+    .setColor(GAME_COLORS.WIN) // Poker embed defaults to green
     .setDescription(
       `Board Cards\n${board.join(" | ")}\n\n` +
         `Your Cards\n${playerCards.join(" | ")}\n\n` +
