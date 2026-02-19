629eff6e3f2bace78c7036a95dae11f612bbc0f2
Hypergamer6899A <bj951630@gmail.com>
2026-02-19 09:35:01 -0600

    Update balance.js

--- commands/g/balance.js
+++ commands/g/balance.js
@@ -1,9 +1,13 @@
 import { EmbedBuilder } from "discord.js";
 import { getUser } from "../services/userCache.js";
+import { getHouse } from "../utils/house.js";
 import { UTIL_COLORS } from "../utils/embedColors.js";
 
 export async function balanceCommand(client, message) {
   const user = await getUser(message.author.id);
+
+  // Load Gambler pot
+  const house = await getHouse();
 
   return message.reply({
     embeds: [
@@ -12,7 +16,9 @@ export async function balanceCommand(client, message) {
         .setColor(UTIL_COLORS.BLURPLE)
         .setDescription(
           `**Current Wallet**\n` +
-            `You currently have **$${user.balance}**.`
+            `You currently have **$${user.balance}**.\n\n` +
+            `**Server Jackpot Pot**\n` +
+            `**$${house.jackpotPot}**`
         )
     ]
   });
