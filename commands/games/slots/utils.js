ee988d7cb8041ebe57a92440dd8bdff876f63f3d
Hypergamer6899A <bj951630@gmail.com>
2026-02-19 09:05:56 -0600

    Update utils.js

--- commands/games/slots/utils.js
+++ commands/games/slots/utils.js
@@ -2,11 +2,11 @@
 
 // Slot emoji pool with weighted chances
 const SYMBOLS = [
-  { emoji: "🍒", weight: 40, multiplier: 1 },
-  { emoji: "🍋", weight: 30, multiplier: 0.5 },
-  { emoji: "🍇", weight: 20, multiplier: 3 },
-  { emoji: "💎", weight: 8, multiplier: 5 },
-  { emoji: "👑", weight: 2, multiplier: 10 }
+  { emoji: "<:testblock:1429946118229196810>", weight: 40, multiplier: 1 },
+  { emoji: "<:Warden:1429946036809371769>", weight: 30, multiplier: 0.5 },
+  { emoji: "<:scaryhorrormonster:1429946136784932864>", weight: 20, multiplier: 3 },
+  { emoji: "<:sus:1429945939006853170>", weight: 8, multiplier: 5 },
+  { emoji: "<:waxedlightlyweatheredcutcopperst:1429946087921287168>", weight: 2, multiplier: 10 }
 ];
 
 // Weighted random picker
