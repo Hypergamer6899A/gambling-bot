15b99c95ea89ac796202d76df64b9576403d833a
Hypergamer6899A <bj951630@gmail.com>
2026-03-21 10:54:34 -0500

    Update gift.js

--- commands/g/gift.js
+++ commands/g/gift.js
@@ -1,3 +1,4 @@
+// commands/g/gift.js
 import { getUser, saveUser } from "../services/userCache.js";
 
 export async function giftCommand(client, message, args) {
@@ -6,28 +7,26 @@ export async function giftCommand(client, message, args) {
 
   if (!target)
     return message.reply("Tag someone to gift money to.");
-
   if (isNaN(amount) || amount <= 0)
     return message.reply("Enter a valid amount.");
-
   if (target.id === message.author.id)
     return message.reply("You can't gift money to yourself.");
+  if (target.bot)
+    return message.reply("You can't gift money to a bot.");
 
-  const sender = await getUser(message.author.id);
+  const sender   = await getUser(message.author.id);
+  const receiver = await getUser(target.id);
 
   if (sender.balance < amount)
     return message.reply("You don't have enough money.");
 
-  const receiver = await getUser(target.id);
-
-  sender.balance -= amount;
+  sender.balance   -= amount;
   receiver.balance += amount;
 
   await saveUser(message.author.id, sender);
   await saveUser(target.id, receiver);
 
-  message.reply(
-    `You gifted **$${amount}** to ${target}. They now have **$${receiver.balance}**.`
+  return message.reply(
+    `Sent **$${amount}** to ${target}. They now have **$${receiver.balance}**.`,
   );
 }
-
