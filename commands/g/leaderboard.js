25d85ac776c9b46590e4eff96cabaadec2391825
Hypergamer6899A <bj951630@gmail.com>
2026-03-21 10:58:00 -0500

    Update leaderboard.js

--- commands/g/leaderboard.js
+++ commands/g/leaderboard.js
@@ -1,16 +1,18 @@
 // commands/g/leaderboard.js
 import { getFirestore } from "firebase-admin/firestore";
+import { getHouse } from "../utils/house.js";
 import { leaderboardEmbed } from "../utils/embeds.js";
 
 const db     = getFirestore();
 const BOT_ID = process.env.BOT_ID;
 
 export async function leaderboardCommand(client, message) {
-  const snap = await db.collection("users")
-    .orderBy("balance", "desc")
-    .get();
+  const [snap, house] = await Promise.all([
+    db.collection("users").orderBy("balance", "desc").get(),
+    getHouse(),
+  ]);
 
-  // Filter out the house account
+  // Filter out the house account from player rankings
   const allUsers = snap.docs
     .filter(doc => doc.id !== BOT_ID)
     .map(doc => ({ id: doc.id, balance: doc.data().balance }));
@@ -22,5 +24,5 @@ export async function leaderboardCommand(client, message) {
     ? { ...allUsers[callerRank], rank: callerRank + 1 }
     : null;
 
-  return message.reply({ embeds: [leaderboardEmbed(top5, callerEntry)] });
+  return message.reply({ embeds: [leaderboardEmbed(top5, callerEntry, house)] });
 }
